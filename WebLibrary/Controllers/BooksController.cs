41534cb465f7875f484c90a5fc33861190dc432f
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-06-10 11:26:10 +0300

    async CRUD

--- WebLibrary/Controllers/BooksController.cs
+++ WebLibrary/Controllers/BooksController.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebLibrary.Constants;
 using WebLibrary.EFContext;
+using WebLibrary.EFContext.Data;
 using WebLibrary.Models;
 
 namespace WebGallery.Controllers
@@ -25,27 +26,79 @@ namespace WebGallery.Controllers
         }
         [HttpGet]
         [Route("search")]
-        public IActionResult SearchBooks()
+        public async Task <IActionResult> SearchBooks()
         {
-            var list = _context.Books.ToList();
-            return Ok(list);
+            return await Task.Run(() =>
+            {
+                return Ok(_context.Books.AsQueryable());
+            });
+           
         }
 
-        //[HttpPost]
-        //[Route("add")]
-        ////[Authorize(Roles = Roles.Admin)]
-        //public IActionResult AddBook([FromBody] BookViewModelsError book)
-        //{
-        //    _context.Books.Add(book);
-        //    _context.SaveChanges();
-        //    //var dir = Directory.GetCurrentDirectory();
-        //    //var dirSave = Path.Combine(dir, "uploads");
-        //    //var imageName = Path.GetRandomFileName() + ".jpg";
-        //    //var imageSaveFolder = Path.Combine(dirSave, imageName);
-        //    //var image = car.Image.LoadBase64();
-        //    //image.Save(imageSaveFolder);
-        //    return Ok(new { message="Додано" });
-        //}
+        [HttpPost]
+        [Route("add")]
+        //[Authorize(Roles = Roles.Admin)]
+        public async Task<IActionResult> AddBook([FromBody] Book book)
+        {
+            return await Task.Run(() =>
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return Ok(new { message = "Додано" });
+            });
+            
+           
+           
+            //var dir = Directory.GetCurrentDirectory();
+            //var dirSave = Path.Combine(dir, "uploads");
+            //var imageName = Path.GetRandomFileName() + ".jpg";
+            //var imageSaveFolder = Path.Combine(dirSave, imageName);
+            //var image = car.Image.LoadBase64();
+            //image.Save(imageSaveFolder);
+          
+        }
 
+
+        [HttpPut("{id}")]
+        //[Route("update")]
+        public async Task<IActionResult> UpdateBook([FromRoute] int id, [FromBody] Book book)
+        {
+            return await Task.Run(() => {
+                //  Отримання елемента для редагування
+                var books = _context.Books.FirstOrDefault(x => x.Id == id);
+                //  Перевірка чи обєкт не пустий
+                if (books != null)
+                {
+                    //  Редагування
+                    books.Name = book.Name;
+                    books.Author = book.Author;
+                    books.Description = book.Description;
+                    books.Year = book.Year;
+                   
+                    //  Збереження змін
+                    _context.SaveChanges();
+                }
+
+                return Ok(new { message = "Дані відредаговано!" });
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook([FromRoute] int id)
+        {
+            return await Task.Run(() =>
+            {
+
+            
+            var books = _context.Books.SingleOrDefault(x => x.Id == id);
+            if (books != null)
+            {
+                    _context.Remove(books);
+                    _context.SaveChanges();
+                }
+
+            return Ok("Книга видалена");
+            });
+        }
     }
 }
